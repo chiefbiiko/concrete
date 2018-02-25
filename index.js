706d354a06d466dfd0c78f247899caa1a2c656f6
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2018-02-25 08:51:51 +0100

    balled up

--- index.js
+++ index.js
@@ -1,56 +1,18 @@
 var fs = require('fs')
 var http = require('http')
-var path = require('path')
-var zlib = require('zlib')
 
-var HOST = process.argv[3] || '127.0.0.1'
-var PORT = parseInt(process.argv[2]) || 50000
+var HOST = process.env.HOST || process.argv[2] || '127.0.0.1'
+var PORT = parseInt(process.env.PORT) || parseInt(process.argv[3]) || 41900
+var FILE = process.env.BALL || process.argv[4]
+var BALL = fs.readFileSync(FILE)
+var HEADERS = { 'content-encoding': 'gzip', 'content-type': 'text/html' }
 
-var RESOURCES = path.join(__dirname, 'RESOURCES')
-
-var HTML = loadGzipBufferSync(path.join(RESOURCES, 'index.html'))
-var JS   = loadGzipBufferSync(path.join(RESOURCES, 'app.js'))
-var CSS  = loadGzipBufferSync(path.join(RESOURCES, 'style.css'))
-
-var PICS = new Map(
-  fs.readdirSync(path.join(RESOURCES, 'img')).map(function (pic) {
-    return [ pic, loadGzipBufferSync(path.join(RESOURCES, 'img', pic)) ]
-  })
-)
-
-function loadGzipBufferSync(file) {
-  return zlib.gzipSync(fs.readFileSync(file))
+function onrequest (req, res) {
+  res.writeHead(200, HEADERS)
+  res.end(BALL)
 }
 
-function onlisten () {
-  console.log('httpserver listening @ ' + HOST + ':' + PORT)
-}
-
-function router (req, res) { // keep-alive? share tcp socket with chat ws: ?
-  var base = path.basename(req.url)
-
-  res.statusCode = 200
-  res.setHeader('Content-Encoding', 'gzip')
-
-  if (!base || base === '/') {
-    res.setHeader('Content-Type', 'text/html')
-    res.end(HTML)
-  } else if (base === 'app.js') {
-    res.setHeader('Content-Type', 'application/javascript')
-    res.end(JS)
-  } else if (base === 'style.css') {
-    res.setHeader('Content-Type', 'text/css')
-    res.end(CSS)
-  } else if (PICS.has(base)) {
-    res.setHeader('Content-Type', 'image/*')
-    res.end(PICS.get(base))
-  } else {
-    res.statusCode = 404
-    res.end()
-  }
-
-}
-
-var httpserver = http.createServer(router)
+var onlisten = console.log.bind(null, 'httpserver live @ ' + HOST + ':' + PORT)
+var httpserver = http.createServer(onrequest)
 
 httpserver.listen(PORT, HOST, onlisten)
--- resources/app.js
+++ resources/app.js
@@ -1,12 +1,52 @@
 // app.js
 
+function make (tag) {
+  return document.createElement(tag)
+}
+
+function $ (selector) {
+  var nl = document.querySelectorAll(selector)
+  return nl.length === 1 ? nl.item(0) : Array.from(nl)
+}
+
+function appendChildren (parent, ...children) {
+  while (children.length) parent.appendChild(children.shift())
+}
+
 function init () {
-  var txt = document.createElement('p')
-  txt.innerText = 'some fraud'
-  var pic = document.createElement('img')
-  pic.src = '/resources/img/me.jpg'
-  document.body.appendChild(txt)
-  document.body.appendChild(pic)
+  document.body.appendChild(trap.getHeader())
+}
+
+var trap = {
+  _header: null,
+  getHeader () {
+    if (this._header) return this._header
+    this._header = make('div')
+    var logo = make('img')
+    var cart = make('img')
+    var reinigung = make('span')
+    var schutz = make('span')
+    var gestaltung = make('span')
+    var philosophy = make('span')
+    var diy = make('span')
+    this._header.id = 'header'
+    logo.id = 'logo'
+    cart.id = 'nav-cart'
+    reinigung.id = 'nav-reinigung'
+    schutz.id = 'nav-schutz'
+    gestaltung.id = 'nav-gestaltung'
+    philosophy.id = 'nav-philosophy'
+    diy.id = 'nav-diy'
+    logo.alt = 'logo'
+    cart.alt = 'shopping cart'
+    reinigung.innerText = 'Reingung'
+    schutz.innerText = 'Schutz'
+    gestaltung.innerText = 'Gestaltung'
+    philosophy.innerText = 'Philosophy'
+    diy.innerText = 'DIY'
+    appendChildren(this._header, logo, cart, reinigung, schutz, gestaltung, philosophy, diy)
+    return this._header
+  }
 }
 
 window.onload = init
